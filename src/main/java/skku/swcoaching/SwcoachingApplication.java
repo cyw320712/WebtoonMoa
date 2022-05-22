202d2dc8ec1abdb2ee02b99320f2e2a832b38c33
cyw7515@naver.com <cyw7515@naver.com>
2022-05-22 11:20:06 +0900

    선별 과제 1차 release

---
+++ src/main/java/skku/swcoaching/SwcoachingApplication.java
@@ -0,0 +1,13 @@
+package skku.swcoaching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwcoachingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwcoachingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/skku/swcoaching/SwcoachingApplicationTests.java
@@ -0,0 +1,13 @@
+package skku.swcoaching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwcoachingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
